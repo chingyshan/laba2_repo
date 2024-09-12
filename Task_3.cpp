42760291f9836e2574093ff37c54a84f8a58e008
chingyshan <vlad.ag070906@gmail.com>
2024-09-12 20:00:18 +0300

    Added pro

--- Task_3.cpp
+++ Task_3.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include<cmath>
+using namespace std;
 
 int main() {
     double start, end, step;
-    std::cout << "Введите начальное значение x: ";
-    std::cin >> start; // Задаем исходное значение переменной
-    std::cout << "Введите конечное значение x: ";
-    std::cin >> end; // Задаем конечно значение переменной
-    std::cout << "Введите шаг: ";
-    std::cin >> step; // Задаем шаг, с которым с которым будет изменяться аргумент
-    for (double x = start; x <= end; x+= step) {
+    cout << "Введите начальное значение x: ";
+    cin >> start; // Задаем исходное значение переменной
+    cout << "Введите конечное значение x: ";
+    cin >> end; // Задаем конечно значение переменной
+    cout << "Введите шаг: ";
+    cin >> step; // Задаем шаг, с которым с которым будет изменяться аргумент
+    for (double x = start; x <= end; x += step) {
         double y = sin(x); // функция y = sin(x)
-        std::cout << x << " | " << y << std::endl; // Выводим значения функции для каждого аргумента (x | y)
+        cout << x << " | " << y << endl; // Выводим значения функции для каждого аргумента (x | y)
     }
-    std::cout << "noob" << std::endl;
+cout << "pro"<< endl;
     return 0;
 }
